a09df4876e972180ad1cd32fed36297a97fb1f64
Ayush Kumar Jha <jha44481@gmail.com>
2026-03-17 22:04:30 +0530

    fix: remove redundant Toaster to fix duplicate popups

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import { Toaster } from 'react-hot-toast'
 
 export default function Layout({ children, title = 'LearnFlow' }) {
   return (
@@ -11,7 +10,6 @@ export default function Layout({ children, title = 'LearnFlow' }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <div className="min-h-screen bg-gray-50 flex flex-col">
-        <Toaster position="top-center" reverseOrder={false} />
         <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer />
