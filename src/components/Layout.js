af6ee6ffa3f42c54facb1a1004a88b3add1d2a91
Ayush Kumar Jha <jha44481@gmail.com>
2026-03-17 16:19:46 +0530

    feat: integrate professional footer and legal pages

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 export default function Layout({ children, title = 'LearnFlow' }) {
   return (
@@ -8,9 +9,10 @@ export default function Layout({ children, title = 'LearnFlow' }) {
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50 flex flex-col">
         <Navbar />
-        <main>{children}</main>
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </div>
     </>
   )
